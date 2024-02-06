6f65ab552ca4b474f2ee21e411b8df8c2c69605b
shaimaa-git <shaimaasaidmo@gmail.com>
2024-02-06 12:05:20 +0400

    calculate average for three int type value

---
+++ average.java
@@ -0,0 +1,9 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        int num1=23;
+        int num2=21;
+        int num3=45;
+        float average=(float)(num1+num2+num3)/3;
+        System.out.println(average);  
+} 
+}
