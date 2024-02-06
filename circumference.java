9e0296220e9b2c582781f806ef8a1b251e7957df
shaimaa-git <shaimaasaidmo@gmail.com>
2024-02-06 22:22:31 +0400

    calculate circomference

---
+++ circumference.java
@@ -0,0 +1,9 @@
+class HelloWorld {
+    public static void main(String[] args) {
+         double PI=3.14;
+         double raduis=5;
+         double circumference=2*PI*raduis;
+         System.out.println(circumference);
+        
+    }
+}
