ccfc9819b9904c403e69f8288252d19e1b0a8096
shaimaa-git <shaimaasaidmo@gmail.com>
2024-02-06 22:53:11 +0400

    convert from c to f tempreture

---
+++ convertingtempreture.java
@@ -0,0 +1,8 @@
+class HelloWorld {
+    public static void main(String[] args) {
+         int C=37;
+         float F=((float)1.8*C)+32;
+         System.out.println("tempreture in F:"+F);
+        
+    }
+}
