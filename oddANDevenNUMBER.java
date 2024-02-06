622675f71f9a4b87df599d48d2c8609dfc8528ee
shaimaa-git <shaimaasaidmo@gmail.com>
2024-02-06 23:15:56 +0400

    check if the given number is even or odd

---
+++ oddANDevenNUMBER.java
@@ -0,0 +1,10 @@
+
+class HelloWorld {
+    public static void main(String[] args) {
+         int number=13;
+         if (number%2==0)
+           System.out.println("the number "+number+" is even");
+         else
+           System.out.println("the number "+number+" is odd");
+    }
+}
