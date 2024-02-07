c0246ab584942c29ec760fa28638eaa802a5c34a
shaimaa-git <shaimaasaidmo@gmail.com>
2024-02-07 08:40:17 +0400

    converting inch to cm

---
+++ convertingINCHtoCM.java
@@ -0,0 +1,9 @@
+
+class HelloWorld {
+    public static void main(String[] args) {
+    double numberINinche=5;
+    double numberINcm=numberINinche*2.54;
+    System.out.println("5 inch equal to "+numberINcm+" cm");
+    }
+}  }
+}
