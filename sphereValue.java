ebcf973d2e09e642dfb0faa82de442f7ddd8defb
shaimaa-git <shaimaasaidmo@gmail.com>
2024-02-07 09:02:22 +0400

    calculate sphere value

---
+++ sphereValue.java
@@ -0,0 +1,7 @@
+class HelloWorld {
+    public static void main(String[] args) {
+    double R=5;
+    double sphereVolume=(3.0/4.0) * Math.PI * Math.pow(R,3);
+    System.out.println("Value of sphere: "+sphereVolume);
+    }
+}
