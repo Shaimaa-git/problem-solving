57dbf2a8f01df828b61f76faa9343e0634bb1b20
shaimaa-git <shaimaasaidmo@gmail.com>
2024-02-07 10:36:40 +0400

    calculate traingle area

---
+++ traingleofarea.java
@@ -0,0 +1,8 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        double base=3;
+        double height=9;
+        double traingleArea=(0.5*base*height);
+        System.out.println("The area of traingle is :"+traingleArea);
+    }
+}
