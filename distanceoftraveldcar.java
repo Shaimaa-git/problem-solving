9a93eda25ab35110da7a93b1eeab1cf5faf98c96
shaimaa-git <shaimaasaidmo@gmail.com>
2024-02-07 11:14:29 +0400

    calculate the distance of a cer in different speeds

---
+++ distanceoftraveldcar.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        double speed1=7;
+        double speed2=9;
+        double averagespeed=(speed1+speed2)/2;
+        double time=4;
+        double distance=averagespeed*time;
+        System.out.println("The distance is :"+distance);
+    }
+}
