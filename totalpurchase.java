478b7f6f4949e10ab3b1cbd71991c5182d0c19d1
shaimaa-git <shaimaasaidmo@gmail.com>
2024-02-07 10:47:06 +0400

    calculate total purchase for three items

---
+++ totalpurchase.java
@@ -0,0 +1,10 @@
+
+class HelloWorld {
+    public static void main(String[] args) {
+        double item1=4;
+        double item2=9;
+        double item3=6;
+        double totalprice=item1+item2+item3;;
+        System.out.println("The total purchase is :"+totalprice);
+    }
+}
