928fe5ee0574c8d8af8359089cd50aa29ffb0d5c
shaimaa-git <shaimaasaidmo@gmail.com>
2024-02-07 07:57:16 +0400

    calculate rectangle area

---
+++ rectangleAreaCalculate.java
@@ -0,0 +1,9 @@
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int length=67;
+        int width=23;
+        int result= length*width;
+        System.out.println(result);
+    }
+}
